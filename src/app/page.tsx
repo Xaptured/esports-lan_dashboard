98033661d606b712f2c5905db0b9e930381eef30
Jack <jk19011999@gmail.com>
2024-08-25 19:08:45 +0530

    Section-2: Added and setup of second section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import { GridBackgroundDemo } from '@/components/hero/background-demo';
+import AboutUs from '@/components/about-us/about-us';
 import Hero from '@/components/hero/hero';
 import NavigationBar from '@/components/navigation-bar/navigation-bar';
-import ThemeButton from '@/components/theme/theme-button';
 
 export default function Home() {
   return (
     <>
       <NavigationBar />
       <Hero />
+      <AboutUs />
     </>
   );
 }
